a41133b7b5ac75220012beb0b5bd755320977509
mauroporzio <mauroporzio02@gmail.com>
2021-05-23 19:48:22 -0300

    Push comentarios codigo
    Se comento la libreria del logger para que se comprenda mejor que hace cada parte de dicha clase y su funcion.

--- Logger/LogHelper/Logger.cs
+++ Logger/LogHelper/Logger.cs
@@ -6,11 +6,11 @@ namespace LogHelper
 {
     public class Logger : ILogger
     {
-        public string logPath { get; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigurationManager.AppSettings["logPath"]);
-        public void log(string errorMessage)
+        public string logPath { get; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigurationManager.AppSettings["logPath"]); // combina el path de donde se encuentra el dominio de la app y se le agrega desde una key del web.config el nombre del archivo a crear o modificar.
+        public void log(string errorMessage) // se recibe el error a logear por parametro.
         {
-            DateTime now = DateTime.Now;
-            File.AppendAllText(logPath, string.Concat(now.ToString()," ",errorMessage, Environment.NewLine));
+            DateTime now = DateTime.Now; // se guarda la fecha y hora para luego agregarla al log.
+            File.AppendAllText(logPath, string.Concat(now.ToString()," ",errorMessage, Environment.NewLine)); // si el archivo no existe se crea, carga el error y luego se guarda, pero sino solo agrega el error al log ya creado anteriormente.
         }
     }
 }
