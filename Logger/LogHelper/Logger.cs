a8bec477615e22cfc2523e2913bbfdccd539bb41
mauroporzio <mauroporzio02@gmail.com>
2021-05-23 19:44:27 -0300

    Initial commit

---
+++ Logger/LogHelper/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+
+namespace LogHelper
+{
+    public class Logger : ILogger
+    {
+        public string logPath { get; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigurationManager.AppSettings["logPath"]);
+        public void log(string errorMessage)
+        {
+            DateTime now = DateTime.Now;
+            File.AppendAllText(logPath, string.Concat(now.ToString()," ",errorMessage, Environment.NewLine));
+        }
+    }
+}
